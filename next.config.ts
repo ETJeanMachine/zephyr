71556152a2d90cb5c45ffaad32e26587b8de970a
Eric Hamilton <ericjean873@gmail.com>
2025-02-25 15:29:09 -0800

    fixed async issues

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 /** @type {import('next').NextConfig} */
-const nextConfig: import('next').NextConfig = {};
+const nextConfig: import("next").NextConfig = {};
 
- if (process.env.NODE_ENV === 'development') {
-   await setupDevPlatform();
- }
+if (process.env.NODE_ENV === "development") {
+  (async () => {
+    await setupDevPlatform();
+  })();
+}
 
 export default nextConfig;
